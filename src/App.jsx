b8ff300036b014dac7c3475431cea643aa424f98
Nathan Gomes <nathang.pulido98@outlook.com>
2025-08-01 11:54:41 -0300

    feat: adicionado componente de visualização de tarefas

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar Programação",
+      description: "Aprofundar os conhecimentos em React",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Estudar Cálculo",
+      description: "Estudar toda a base necessária para ingressar no cálculo",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Estudar algoritmos e estrutura de dados",
+      description:
+        "Revisar a base de algoritmos de ordenação e as principais estrutura de dados e como implementar",
+    },
+  ]);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 text-center">
+          Gerenciador de Tarefas
+        </h1>
+        <Tasks tasks={tasks} />
+        <AddTask />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
